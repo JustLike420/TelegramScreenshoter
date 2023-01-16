348e5e691b775e0faafb1e1e9cb89c4b7e1fed76
Vladimir <justlikevova@gmail.com>
2023-01-17 01:58:05 +0300

    save format

--- utils/screenshoter.py
+++ utils/screenshoter.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 from selenium import webdriver
 import logging
+from data.config import screenshot_path
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
@@ -16,8 +17,13 @@ async def take_screenshot(url: str) -> tuple[bool, str]:
         url = 'https://' + url
     try:
         driver.get(url)
-        name = url.replace("https://", "") + ".png"
-        driver.save_screenshot(name)
+        if not os.path.exists('screenshots'):
+            os.mkdir('screenshots')
+        now = datetime.now()
+        date_now = now.strftime('%Y-%m-%d_%H_%M')
+        name = screenshot_path + date_now + '_' + url.replace("https://", "").replace('.', '_') + ".png"
+        # driver.save_screenshot(name)
+        driver.save_full_page_screenshot(name)
         status = True
     except Exception as e:
         logging.error(e)
